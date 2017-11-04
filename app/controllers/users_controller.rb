4a66769a287009c580924ca5d353d8ea2d522f29
sarzana42 <sarzana42@gmail.com>
2017-11-04 12:08:35 +0000

    2017110421

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -10,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ようこそ！レイホープへ"
       redirect_to @user
     else
       render 'new'
@@ -20,11 +23,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: '編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
   
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+    def set_user
+      @user = User.find(params[:id])
+    end
   
 end
