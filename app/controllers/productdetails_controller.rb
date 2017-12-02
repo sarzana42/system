dd30ae1a288e9a71d20e9c46dd1ce34cd03621b6
sarzana42 <sarzana42@gmail.com>
2017-12-02 13:41:46 +0000

    making productdetail

--- app/controllers/productdetails_controller.rb
+++ app/controllers/productdetails_controller.rb
@@ -1,16 +1,17 @@
 class ProductdetailsController < ApplicationController
     before_action :logged_in_user, only: [:create]
+    before_action :set_productdetail, only: [:show, :edit, :update, :destroy]
     
   def index
-    @productdetail = Productdetail.all
+    @productdetails = Productdetail.all
   end
   
   def new
     @productdetail = Productdetail.new
+    
   end
   
   def show
-    @productdetail = Productdetail.find(params[:id])
   end
 
 
@@ -18,14 +19,36 @@ class ProductdetailsController < ApplicationController
     @productdetail = Productdetail.new(productdetail_params)
     if @productdetail.save
       flash[:success] = "productdetail created!"
-      redirect_to root_url
+      redirect_to @productdetail
     else
-      render 'static_pages/home'
+      @productdetails = Productdetail.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'new'
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @productdetail.update(productdetail_params)
+      redirect_to productdetail_path, notice: '編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
+
+  
+  
   private
   def productdetail_params
-    params.require(:productdetail).permit(:id, :colorcode, :colorname, :sizecode, :sizename, :vprice, :wprice, :price, :stock, {:product_ids => []})
+    params.require(:productdetail).permit(:id, :product_id, :colorcode, :colorname, :sizecode, :sizename, :vprice, :wprice, :price, :stock)
   end
+  
+  def set_productdetail
+    @productdetail = Productdetail.find(params[:id])
+  end
+  
+  
 end
